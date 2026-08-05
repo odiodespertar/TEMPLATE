a6f046089912830d2551317e585d53f329413a75
odiodespertar <odiodespertar@gmail.com>
2026-08-05 04:09:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import pandas as pd
 import io
 from streamlit.components.v1 import html     
 
-
-
 st.set_page_config(page_title="Monitor Logístico - Liliana García", layout="wide", initial_sidebar_state="expanded")
+
+
+# --- INICIALIZADOR DE RUTEOS DINÁMICOS ---
+if "ruteos_dinamicos" not in st.session_state:
+    st.session_state["ruteos_dinamicos"] = {}
+
+
 
 # ==========================================
 # CSS GENERAL + ESTILO DE VENTANA FLOTANTE
