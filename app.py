29b4cc961eaa3ca016cd46eae07daabefeff7f7d
odiodespertar <odiodespertar@gmail.com>
2026-08-09 15:13:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5350,7 +5350,7 @@ function iniciarArrastreFlotante(e) {{
         </div>
 
         <div style="display: flex; gap: 6px; margin-top: 10px;">
-            <input type="text" id="input-bot-lateral" placeholder="Escribe tu consulta..." onkeydown="if(event.key==='Enter') enviarConsultaBotLateral()" style="flex: 1; padding: 8px 10px; border-radius: 6px; border: 1px solid #444; background: #25282b; color: white; font-size: 16px;">
+            <input type="text" id="input-bot-lateral" placeholder="Escribe tu consulta..." onkeydown="if(event.key==='Enter') enviarConsultaBotLateral()" style="flex: 1; padding: 8px 10px; border-radius: 6px; border: 1px solid #444; background: #4169E1; color: white; font-size: 16px;">
             <button onclick="enviarConsultaBotLateral()" style="cursor: pointer; background: #ffef40; color: white; border: none; padding: 8px 12px; border-radius: 6px; font-weight: bold; font-size: 14px;">🚀</button>
         </div>
     </div>
