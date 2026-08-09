3fea27ab0465add17d89a0de615b59fefbecd801
odiodespertar <odiodespertar@gmail.com>
2026-08-09 11:42:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1133,7 +1133,7 @@ PERFILES = {}
 perfil_actual = "LUNES"
 
 # 🟢 Convertimos los diccionarios de reglas.py a JSON para que JavaScript los lea completos
-reglas_json = json.dumps(reglas_ruteo)
+reglas_ruteo__json = json.dumps(reglas_ruteo)
 mapa_origenes_json = json.dumps(MAPA_ORIGENES)
 preguntas_faq_json = json.dumps(PREGUNTAS_FRECUENTES)
 
